761d4f6be73574b6280ebd1022caaa4d60b07b51
Brian Austin <brianjaustin@gmail.com>
2019-02-18 16:22:24 -0500

    Basic tournament list and detail

---
+++ tournament/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.TournamentList.as_view(), name='tournament_list'),
+
+    # Tournament model urls
+    path('tournament/', views.TournamentList.as_view(), name='tournament_list'),
+    path('tournament/<int:pk>', views.TournamentDetail.as_view(), name='tournament_detail'),
+]
--- tournament/views.py
+++ tournament/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.views import generic
+
+from .models import Tournament
+
+
+class TournamentList(generic.ListView):
+    """
+    Displays a list of tournaments.
+    """
+    model = Tournament
+    context_object_name = 'tournaments'
+
+
+class TournamentDetail(generic.DetailView):
+    """
+    Displays detail for a specific tournament, determined by the primary
+    key given.
+    """
+    model = Tournament
+    context_object_name = 'tournament'
